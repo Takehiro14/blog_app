9ea276e051a33ba2d73d7ae3d8fd9b2e9ca5f1b4
takehiro-tsukagoshi <takehiro.plusultra@gmail.com>
2020-04-13 19:36:12 +0900

    index.scss 微調整

--- db/migrate/20200409031449_create_posts.rb
+++ db/migrate/20200409031449_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.text :title,   null: false
       t.text :content, null: false
+      t.references :user, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,10 @@ ActiveRecord::Schema.define(version: 2020_04_09_042342) do
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title", null: false
     t.text "content", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
